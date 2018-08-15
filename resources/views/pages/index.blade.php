c192326f212910c3baba7f9e69204898be285fef
sari <sari@mail>
2018-08-15 16:20:21 +0300

    first commit 1

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name','LSAPP')}}</title>
+    </head>
+    <body>
+        <h1>welcome to laravel </h1>
+        <p> this is the laravel application from the laravel from scrach youtube serive </p>
+    </body>
+</html>
